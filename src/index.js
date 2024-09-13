6a131177f1b0361f7523ecc566fbc41e0a3ec748
SerAnatoliy <anatolii@maximustribe.com>
2024-09-13 16:06:33 +0200

    order fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store/store';
-import App from './components/App/App';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
