d23b9104e151dab63bd3f781aa67b5ed371b4993
SerAnatoliy <anatolii@maximustribe.com>
2024-09-16 21:25:16 +0200

    remove logs

--- src/api/campersAPI.js
+++ src/api/campersAPI.js
@@ -4,12 +4,12 @@ axios.defaults.baseURL = 'https://66b1f8e71ca8ad33d4f5f63e.mockapi.io';
 
 export const getCampers = async (page = 1) => {
   const data = await axios.get(`/campers/?page=${page}&limit=4`);
-  console.log(data.data.items);
+
   return data.data.items;
 };
 
 export const getAllCampers = async () => {
   const data = await axios.get('/campers');
-  console.log(data.data);
+
   return data.data;
 };
