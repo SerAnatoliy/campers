26878238cf5bbceeaacba6c6059cecb7bbaccc75
SerAnatoliy <anatolii@maximustribe.com>
2024-09-16 21:55:35 +0200

    deploy process fixes

---
+++ src/components/App1/App.jsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import NavLayout from '../Layout/HeaderLayout';
+import FooterLayout from '../Layout/FooterLayout';
+import { ContainerApp } from './App.module';
+import Loaders from '../Loaders/Loaders';
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const CatalogPage = lazy(() => import('../../pages/CatalogPage/CatalogPage'));
+const FavouritesPage = lazy(() => import('../../pages/FavouritesPage/FavouritesPage'));
+const PageNotFound = lazy(() => import('../../pages/PageNotFound/PageNotFound'));
+
+const App = () => {
+  return (
+    <ContainerApp>
+      <Suspense fallback={<Loaders />}>
+        <Routes>
+          <Route path="/" element={<NavLayout />}>
+            <Route element={<FooterLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="/favourites" element={<FavouritesPage />} />
+            </Route>
+            <Route path="/catalog" element={<CatalogPage />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
+    </ContainerApp>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
+import App from './components/App1/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
